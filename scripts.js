e3ea93f2c01da7ecf08b80ae51d2717d45ebef93
Hákon Örn Harðarson <hoh59@hi.is>
2024-10-17 22:53:40 +0000

    Scriptur útfærðar

--- scripts.js
+++ scripts.js
@@ -8,10 +8,10 @@
 //------------------------------------------------------------------------------
 // Fastar
 
-/** Íslenskir sérhljóðar */
+/** Íslenskir samhljóðar */
 const CONSONANTS = 'bcdfghjklmnpqrstvwxz'.split('');
 
-/** Íslenskir samhljóðar */
+/** Íslenskir sérhljóðar */
 const VOWELS = 'aeiouyáéýúíóöæ'.split('');
 
 //------------------------------------------------------------------------------
@@ -49,32 +49,171 @@ function split(str, separator = ' ') {
 // Grunnföll sem skilgreina á
 
 function longest(str) {
-  // Útfæra
+  if (!isString(str)) {
+    return null;
+    // null ef ekki strengur
+  }
+  if (str.length === 0) {
+    return '';
+    // skilar tómum streng ef slegið inn tóman streng
+  }
+  const words = str.split(' '); 
+  let longestword = '';
+  for (const word of words) {
+    if (word.length > longestword.length) {
+      longestword = word;
+    }    
+  }
+  return longestword;
 }
+console.assert(longest("Hnetusmjör og sulta") === "Hnetusmjör", 'longest: should return "quick"');
+console.assert(longest("Halló!!! heimur!!") === "Halló!!!", 'longest: Á að skila "Halló!!!"');
+console.assert(longest(0) === null, 'longest: Á að skila null ef ekki strengur');
+console.assert(longest("") === "", 'longest: Á að skila tómum streng ef input er tómt');
 
 function shortest(str) {
-  // Útfæra
+  if (!isString(str)) {
+    return null;
+    // null ef ekki strengur
+  }
+  if (str.length === 0) {
+    return '';
+    // skilar tómum streng ef slegið inn tóman streng
+  }
+  const words = str.split(' '); 
+  let shortestWord = words[0];
+  for (const word of words) {
+    if (word.length < shortestWord.length) {
+      shortestWord = word;
+    }
+  }
+  return shortestWord;
 }
+console.assert(shortest("Hnetusmjör og sulta") === "og", 'shortest: Á að skila "og"');
+console.assert(shortest("Halló!!! heimur!") === "heimur!", 'shortest: Á að skila "heimur!"');
+console.assert(shortest(0) === null, 'shortest: Á að skila null ef ekki strengur');
+console.assert(shortest("") === "", 'shortest: Á að skila tómum streng ef input er tómt');
 
 function reverse(str) {
-  // Útfæra
+  if (isString(str)) {
+  const split = str.split('');
+  const reversed = split.reverse();
+
+  return reversed.join('');
+  }
+  return null;
 }
+console.assert(reverse('Halló Heimur') === 'rumieH óllaH', 'reverse: snýr við einföldum streng');
+console.assert(reverse(false) === null, 'reverse: ef ekki strengur, skila null');
 
 function palindrome(str) {
-  // Útfæra
+  if (isString(str) && str !== ''){
+    const reversed = reverse(str)
+    return str.toLowerCase() === reversed.toLowerCase()
+  }
+    return false;
 }
+console.assert(palindrome('hahah') === true, 'palindrome: strengur er ekki samhverfur');
+console.assert(palindrome('Halló') === false, 'palindrome: strengur er samhverfur');
+console.assert(palindrome('') === false, 'palindrome: tómur strengur gildur');
 
 function vowels(str) {
-  // Útfæra
+  if (str.length === 0) {
+    return '0';
+    //Skilar '0' ef strengur er tómur
+  }
+  if (!isString(str)) {
+    return null;
+    //Skilar null ef ekki strengur
+  }
+  let countVowels = 0;
+  const strlowercase = str.toLowerCase(); //Því case sensitivity skiptir ekki máli.
+
+  for (const char of strlowercase) {
+    if (VOWELS.includes(char)) {
+      countVowels++;
+    }
+  }
+  if (countVowels > 0) {
+    return countVowels.toString();
+  } else {
+    return '0';
+  }
 }
+console.assert(vowels("HallÓ Heimur") === '5', 'vowels: Á að skila "5"');
+console.assert(vowels("sdfghjkl") === '0', 'vowels: Á að skila "0" fyrir enga sérhljóða');
+console.assert(vowels("") === '0', 'vowels: Á að skila "0" fyrir tóman streng');
+console.assert(vowels(12345) === null, 'vowels: Á að skila null því ekki strengur');
 
 function consonants(str) {
-  // Útfæra
+  if (str.length === 0) {
+    return '0';
+    //Skilar '0' ef strengur er tómur
+  }
+  if (!isString(str)) {
+    return null;
+    //Skilar null ef ekki strengur
+  }
+  let countConsonants = 0;
+  const strlowercase = str.toLowerCase(); //Því case sensitivity skiptir ekki máli.
+
+  for (const char of strlowercase) {
+    if (CONSONANTS.includes(char)) {
+      countConsonants++;
+    }
+  }
+  if (countConsonants > 0) {
+    return countConsonants.toString();
+  } else {
+    return '0';
+  }
 }
+console.assert(consonants("HallÓ Heimur") === '6', 'consonants: Á að skila "6"');
+console.assert(consonants("eIÓæ") === '0', 'consonants: Á að skila "0" fyrir enga samhljóða');
+console.assert(consonants("") === '0', 'consonants: Á að skila "0" fyrir tóman streng');
+console.assert(consonants(12345) === null, 'consonants: Á að skila null því ekki strengur');
+
 
 //------------------------------------------------------------------------------
 // Leiðbeint ferli
 
 function start() {
-  // Útfæra
+  
+  alert("Velkomin í forritið! Þetta forrit telur lengstu og stystu orðin, snýr strengjum við, telur sérhljóða og samhljóða, og athugar hvort strengir séu samhverfir.");
+
+
+  while (true) {
+    const input = prompt('Sláðu inn streng eða smelltu á "CANCEL" til að hætta');
+
+    if (input === null || input.length === 0) {
+      // Ef cancel eða tómt strengur þá gerum við ekki neitt
+      break;
+    }
+
+    //Köllum á föllin
+    const longestWord = longest(input);
+    const shortestWord = shortest(input);
+    const reversedString = reverse(input);
+    const countVowels = vowels(input);
+    const countConsonants = consonants(input);
+    const isPalindrome = palindrome(input);
+
+    alert(`
+      Niðurstöður:
+      Lengsta orðið: ${longestWord}
+      Stysta orðið: ${shortestWord}
+      Snúið streng: ${reversedString}
+      Fjöldi sérhljóða: ${countVowels}
+      Fjöldi samhljóða: ${countConsonants}
+      Er samhverfur? ${isPalindrome}
+      `);
+    
+    // Spyrja notandann hvort þeir vilja spila aftur
+     if (!confirm('Viltu spila aftur?')) {
+       break; // Hætta ef notandi velur ekki að spila aftur
+     }
+      
+  }
+
+  alert("Takk fyrir að nota forritið!");
 }
